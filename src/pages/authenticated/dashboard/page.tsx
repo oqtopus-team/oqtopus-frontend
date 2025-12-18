3052fe39bee81fb76332854bf7f465aec8b990bd
szksrt <szksrt@gmail.com>
2025-12-18 11:10:28 +0900

    fix: adjust dashboard grid layout

--- src/pages/authenticated/dashboard/page.tsx
+++ src/pages/authenticated/dashboard/page.tsx
@@ -42,16 +42,16 @@ export default function Page() {
           'gap-5'
         )}
       >
-        <Card className={clsx(['col-start-1', 'col-end-1'])}>
+        <Card className={clsx(['col-start-1', 'col-end-2'])}>
           <Composer />
         </Card>
         <Card className={clsx(['col-start-2', 'col-end-3', 'overflow-x-auto'])}>
           <DeviceList />
         </Card>
-        <Card className={clsx(['col-start-3', 'col-end-4', 'row-start-1', 'row-end-1  '])}>
+        <Card className={clsx(['col-start-3', 'col-end-4', 'row-start-1', 'row-end-2'])}>
           <Announcements style={{ post: { '--base-size': 0.55 } }} />
         </Card>
-        <Card className={clsx(['col-start-1', 'col-end-3', 'row-start-2', 'overflow-x-auto'])}>
+        <Card className={clsx(['col-start-1', 'col-end-4', 'row-start-2', 'overflow-x-auto'])}>
           <JobList jobs={jobs ?? []} />
         </Card>
       </div>
