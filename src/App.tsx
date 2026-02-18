f77c4556e2d4c1b334c100a5fd0590b653f7d287
Adrian Gaj <adrian.gaj@fujitsu.com>
2026-02-18 10:18:10 +0100

    refactor: remove newline

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,6 @@ import AuthenticatedLayout from '@/pages/authenticated/layout';
 import AuthenticatedDefaultLayout from '@/pages/authenticated/layout_default';
 import DashboardLayout from '@/pages/authenticated/layout_dashboard';
 import { SettingsPage } from '@/pages/authenticated/settings/page';
-
 // ログイン後の画面はlazyload
 const JobList = lazy(async () => await import('@/pages/authenticated/jobs/page'));
 const JobDetail = lazy(async () => await import('@/pages/authenticated/jobs/detail/page'));
