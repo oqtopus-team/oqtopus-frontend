71687e96b0b2fcd3150cf3e184d5440de298142c
Katsujukou Kineya <katsujukou@gmail.com>
2026-03-18 18:55:41 +0900

    Fix : render pauli spaces

--- src/pages/authenticated/composer/page.tsx
+++ src/pages/authenticated/composer/page.tsx
@@ -28,11 +28,11 @@ const renderOperator = (obs: Observable): JobsOperatorItem[] => {
           case 'x':
           case 'y':
           case 'z':
-            return `${prev}${gate._tag.toUpperCase()}${j}`;
+            return `${prev} ${gate._tag.toUpperCase()}${j}`;
 
           case 'emptyCell':
           case 'i':
-            return `${prev}I${j}`;
+            return `${prev} I${j}`;
           default:
             throw new Error('Unexpected gate in the operator!');
         }
